abbbe4aabe1456e9fb8f5244524b1a0e874eee2a
Ikramullah <ikramullah@mplus.software>
2024-07-17 19:44:06 +0700

    feat(dev/server): authorization for room route

--- server/MDrop.Broker/Controllers/Middlewares/VerifyPrivateModeTokenPipeline.cs
+++ server/MDrop.Broker/Controllers/Middlewares/VerifyPrivateModeTokenPipeline.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace MDrop.Broker.Controllers.Middlewares;
 public class VerifyPrivateModeTokenPipeline
 {
@@ -5,6 +8,8 @@ public class VerifyPrivateModeTokenPipeline
     {
         app.Use(async (context, next) => 
         {
+            var response = new VerifyReturnJson();
+
             var privToken = Constant.PrivateModeToken;
             if (privToken == "") 
             {
@@ -13,8 +18,33 @@ public class VerifyPrivateModeTokenPipeline
             }
 
             var token = context.Request.Headers.Authorization;
+            var tokenArr = token.ToString().Split(' ');
+            if (tokenArr.Length == 1)
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = 401;
+                response.Message = "Authorization needed.";
+                await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+                return;
+            }
+
+            token = tokenArr[1];
+            if (token != privToken)
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = 401;
+                response.Message = "Token is invalid.";
+                await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+                return;
+            }
 
             await next();
         });
+    }
+
+    private class VerifyReturnJson
+    {
+        [JsonPropertyName("message")]
+        public string Message { get; set; } = "";
     }
 }
