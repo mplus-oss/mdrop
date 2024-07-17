61c9826272be302e4d60503f2a2ebf7cb310243b
Ikramullah <ikramullah@mplus.software>
2024-07-17 20:16:43 +0700

    fix(dev/server): trimming unnecesarry variable refactor(dev/server): wrap custom context return as a function

--- server/MDrop.Broker/Functions/CustomContentResult.cs
+++ server/MDrop.Broker/Functions/CustomContentResult.cs
@@ -4,6 +4,13 @@ using Microsoft.AspNetCore.Mvc;
 namespace MDrop.Broker.Functions;
 public static class CustomContentResult
 {
+    public static async Task JsonCustomContextResult<T>(HttpContext context, int statusCode, T content) =>
+        await CustomContentResult.GenerateCustomContextResult(
+            context,
+            statusCode,
+            "application/json",
+            JsonSerializer.Serialize<T>(content));
+
     public static ContentResult JsonCustomResult<T>(int statusCode, T content) =>
         CustomContentResult.GenerateCustomResult(
             statusCode,
@@ -22,4 +29,16 @@ public static class CustomContentResult
             Content = content
         };
     }
+
+    public static async Task GenerateCustomContextResult(
+        HttpContext context,
+        int statusCode,
+        string contentType,
+        string content)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = 401;
+        await context.Response.WriteAsync(content);
+    }
 }
+
