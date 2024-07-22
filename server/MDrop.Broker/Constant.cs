4c7218cc907ad171ff15fc1ce71f233486432a3f
Ikramullah <ikramullah@mplus.software>
2024-07-22 15:16:40 +0700

    feat(dev/server): improving entrypoint for creating base random passowrd for salting certificate (soon)

--- server/MDrop.Broker/Constant.cs
+++ server/MDrop.Broker/Constant.cs
@@ -6,5 +6,8 @@ public static class Constant
     public static readonly string TunnelHost = Environment.GetEnvironmentVariable("TUNNEL_HOST") ?? "127.0.0.1";
     public static readonly int TunnelPort = int.Parse(Environment.GetEnvironmentVariable("TUNNEL_PORT") ?? "2222");
     public static readonly string PrivateModeToken = Environment.GetEnvironmentVariable("PRIVATE_MODE_TOKEN") ?? "";
-    public static X509Certificate2 Certificate = X509Certificate2.CreateFromPemFile("cert.pem", "prikey.pem");
+    public static X509Certificate2 Certificate = X509Certificate2.CreateFromEncryptedPemFile(
+        "cert.pem",
+        Environment.GetEnvironmentVariable("X509_CERTIFICATE_PASSWORD") ?? "",
+        "prikey.pem");
 }
