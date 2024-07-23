91181f799f6163655c248bbe3cde22e3d8a7fba4
Ikramullah Latif <ikramullah@mplus.software>
2024-07-23 15:26:30 +0700

    chore!(improv): add base client app and improving broker response (#3)
    * feat(dev/cli): initialize cli
    
    * refactor(dev/cli): refactoring flag system
    
    * refactor(dev/cli): simplify print usage
    
    * feat(dev/cli): add custom http handler for verifying
    
    * feat(dev/server): add isPublic property on variable
    
    * feat(dev/cli): add status code validation for response
    
    * feat(dev/cli): add status code validation for response
    
    * feat(dev/cli): finalize auth command
    
    * feat(dev/cli): create base command for get
    
    * refactor(dev/cli): formatting auth and add remove default value
    
    * feat(dev/cli): create http request for get token before spawning echo instance
    
    * feat(dev/cli): implementing webserver on get command
    
    * refactor(dev/cli): pruning one file
    
    * feat(dev/image): add docker compose
    
    * fix(dev/server): modify dockerfile
    
    * fix(dev/server): pruning dockerfile and modify csproj
    
    * feat(dev/cli): add message on command before sending file
    
    * feat(dev/cli): add feature to upload
    
    * feat(dev/cli): add progress while uploading
    
    * feat(dev/cli): add slider for progressbar
    
    * feat(dev/cli): add validator for duplicate file
    
    * feat(dev/cli): set response header for keep-alive
    
    * refactor(dev/cli): remove unnecessary file
    
    * feat(dev/cli): create new base command for send
    
    * refactor(dev/server): add tunnel metadata on verify
    
    * feat(dev/server): improving entrypoint for creating base random passowrd for salting certificate (soon)
    
    * refactor(dev/server): add environment on docker compose
    
    * fix(dev/cli): change parsing and add missing parameter
    
    * fix(dev/cli): fixing wrong pinning json
    
    * feat(dev/cli): add ssh tunnel generator args and implement on get variable
    
    * feat(dev/cli): make function to upload file
    fix(dev/cli): create command for gracefully kill tunnel after transfer
    
    * fix(dev/cli): fix error when ssh tunnel is up
    
    * feat(dev/cli): progress bar for uploading
    fix(dev/cli): make receiver only retrive one request from sender

---
+++ client/internal/rw_config_file.go
@@ -0,0 +1,93 @@
+package internal
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+)
+
+var (
+	ConfigFileLocation string
+)
+
+type ConfigFileTunnel struct {
+	Host string	`json:"host"`
+	Port int	`json:"port"`
+}
+
+type ConfigFile struct {
+	Token	string			  `json:"token"`
+	URL		string			  `json:"url"`
+	Tunnel  ConfigFileTunnel  `json:"tunnel"`
+}
+
+func init() {
+	var err error
+
+	ConfigFileLocation, err = os.UserHomeDir()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	ConfigFileLocation += "/.mdrop"
+}
+
+func (c ConfigFile) WriteConfig() error {
+	fmt.Println("Writing config file...")
+	if CheckConfigFileExist() {
+		err := os.Remove(GetConfigPath())
+		if err != nil {
+			return err
+		}
+	}
+
+	strJsonByte, err := json.Marshal(c)
+	if err != nil {
+		return err
+	}
+	strConfig := base64.StdEncoding.EncodeToString(strJsonByte)
+	if c.Token != "" {
+		strConfig += " " + base64.RawStdEncoding.EncodeToString([]byte(c.Token))
+	}
+	err = os.WriteFile(ConfigFileLocation, []byte(strConfig), 0644)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Config file is saved on", ConfigFileLocation)
+	return nil
+}
+
+func (c ConfigFile) ParseConfig(conf* ConfigFile) (error) {
+	if !CheckConfigFileExist() {
+		return errors.New("No config file on local. Please log in first.")
+	}
+	file, err := os.ReadFile(ConfigFileLocation)
+	if err != nil {
+		return err
+	}
+
+	confByte, err := base64.StdEncoding.DecodeString(string(file))
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(confByte, &conf)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetConfigPath() string {
+	return ConfigFileLocation
+}
+
+func CheckConfigFileExist() bool {
+	if _, err := os.Stat(ConfigFileLocation); err != nil {
+		return false
+	}
+	return true
+}
